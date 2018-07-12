69d7b0eafe1d07aa0ea04ca244741457a054b2da
ealmontedb <ealmontedb@gmail.com>
2018-07-12 07:58:11 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'reactstrap';
+import { Alert } from "reactstrap";
+import Example from "./Example";
+import NavBar from "./NavBar";
+import blessjpg from "./Logo Bless edited.jpg";
+import Card from "./Cards";
+
+class App extends Component {
+  state={
+     states: ["tx","ga","IL"]
+
+  }
+  onChangeHandler(e){
+       const v= e.target.value;
+       if(e.target.checked){
+         this.setState(
+       (prevState)=> ( {states: prevState.states.push(v)} )
+         ,console.log(this.state.states))
+
+  }/*else{
+        this.setState(
+           (prevState)=> { return {states: prevState.states.filter(x=>x!==v)};}
+         ,console.log('deleted->'+this.state.states))
+       }*/
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+     {/*     <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+  */}
+
+          <NavBar/>
+          <Example />
+          
+        </header>
+      {/*  <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+  </p>*/}
+       {/* <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"  rel="stylesheet">
+  </link>*/}
+       
+       <div className="Logo-container">
+         <img src={blessjpg} className="Logo" alt="logo" />
+         <div className="description">
+            <h1>Quienes somos? </h1>
+            <p>
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus odio odio, eu commodo elit eleifend ut. Aliquam imperdiet, dui vitae dignissim rhoncus, leo erat vulputate nibh, eget rutrum mauris leo sit amet ex. Mauris faucibus erat semper nunc scelerisque, nec finibus nisi tristique. Nam eget maximus dui. Donec vitae diam pretium, bibendum neque id, vestibulum tellus. Donec massa massa, vestibulum ut finibus non, malesuada id purus. Praesent sagittis facilisis rutrum. Duis urna eros, luctus vitae lorem et, pellentesque porttitor diam. Ut non ligula mollis, malesuada augue nec, aliquam lacus. Nunc commodo et dolor sit amet ultrices. Phasellus ligula velit, tristique ut vulputate quis, finibus dignissim elit. 
+          
+            </p>
+          
+         </div>
+       </div>       
+         <div className="Cards">
+            <Card src="https://images.pexels.com/photos/66354/pexels-photo-66354.jpeg?auto=compress&cs=tinysrgb&h=350"/>
+            <Card src="https://images.pexels.com/photos/185489/pexels-photo-185489.jpeg?auto=compress&cs=tinysrgb&h=350"/>
+            <Card src="https://images.pexels.com/photos/356148/pexels-photo-356148.jpeg?auto=compress&cs=tinysrgb&h=350"/>
+         </div>
+         <div>
+          <ul>
+          {this.state.states.map((s,id)=>{
+            return <li key={id}>
+              {s+' '}<input type="checkbox"  name="subscribe" value={s} onChange={this.onChangeHandler.bind(this)}/>
+            </li>}
+            )
+         }
+          </ul>
+         </div>
+      </div>
+      
+    );
+  }
+}
+
+
+export default App;
---
+++ src/Example.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
+const items = [
+  {
+    src: 'https://images.pexels.com/photos/769024/pexels-photo-769024.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+    altText: 'Slide 1',
+    caption: 'Slide 1'
+  },
+  {
+    src: 'https://images.pexels.com/photos/1144404/pexels-photo-1144404.jpeg?cs=srgb&dl=accessories-beach-beauty-model-1144404.jpg&fm=jpg',
+    altText: 'Slide 2',
+    caption: 'Slide 2'
+  },
+  {
+    src: 'https://images.pexels.com/photos/46288/necklace-jewelry-silver-woman-46288.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+  }
+];
+
+class Example extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
+          >
+          <img src={item.src} alt={item.altText} />
+          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+        interval={3000}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+      </Carousel>
+    );
+  }
+}
+
+
+export default Example;
